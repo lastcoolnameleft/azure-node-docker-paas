60a0c27b5939a32f3a92692c832f963a5dfeafa8
Ben Coleman <benc.uk@gmail.com>
2017-04-08 09:43:14 +0100

    Endless updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const os = require('os');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Demo Azure App' });
+router.get('/', function (req, res, next) {
+  var info = { 
+    release: os.release(), 
+    type: os.type(), 
+    cpus: os.cpus(), 
+    hostname: os.hostname(), 
+    arch: os.arch(),
+    mem: os.totalmem()
+  }
+
+  res.render('index', { title: 'AZure Demo App', info: info });
 });
 
 module.exports = router;
